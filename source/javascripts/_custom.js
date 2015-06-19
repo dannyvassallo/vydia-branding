f4538f92ade913b029f5185e7c6ae4bae37a7642
dannyvassallo <danielvassallo87@gmail.com>
2015-06-19 17:31:15 -0400

    can switch between hex and RGB

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -1,3 +1,20 @@
+// Hex to RGB
+function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
+function hexToRgb(h)
+{
+    var r = parseInt((cutHex(h)).substring(0,2),16), g = ((cutHex(h)).substring(2,4),16), b = parseInt((cutHex(h)).substring(4,6),16)
+    return "rgb(" + r +', '+ b + ', ' + b + ")";
+}
+
+// RGB to Hex
+function rgb2hex(rgb){
+ rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+ return "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2);
+}
+
 // Copy Stuff
 $(function(){
   $('.color').each(function(){
@@ -25,4 +42,21 @@ $(function(){
   var textH = $('.copy-text').height();
   var margin = bodH - (textH / 2);
   $('.copy-text').css('margin-top',margin);
+});
+
+$(function(){
+  $('#hex2rgb').bind('change', function(e){
+    if($('#hex2rgb').val() == 'rgb'){
+      $('.color').each(function(){
+        var hex = $(this).data("clipboard-text");
+        var rgb = hexToRgb(hex);
+        $(this).attr("data-clipboard-text", rgb);
+      });
+    } else {
+      $('.color').each(function(){
+        var rgb = $(this).attr("data-clipboard-text");
+        $(this).attr("data-clipboard-text", rgb2hex(rgb));
+      });
+    }
+  });
 });
